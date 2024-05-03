bc124225f88543a59562d354c2a8f2caee94c13c
Manoj-HT <manulogan101@gmail.com>
2024-05-03 20:39:27 +0530

    9

--- src/Intro/Intro.tsx
+++ src/Intro/Intro.tsx
@@ -79,7 +79,7 @@ const Intro = () => {
 			<div style={{ ...styles.title, opacity: `${opacity}` }}>
 				<Typewriter text="This is my title text" fromToFrame={[250, 370]} />
 			</div>
-			<Audio src={staticFile("intro-music/music.mp3")} />
+			<Audio src={staticFile("intro-music/music.mp3")} endAt={600} />
 		</div>
 	);
 };
